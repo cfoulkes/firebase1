3426aef51825f0fbbd96863f77e840f659c1b864
Colin <cfoulkes@protegra.com>
2016-10-12 19:26:52 -0500

    auth working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable, AuthProviders, AuthMethods } from 'angularfire2';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+  items: FirebaseListObservable<any[]>;
+
+  constructor(private af: AngularFire) {
+    this.af.auth.subscribe(auth => console.log(auth));
+  }
+
+  ngOnInit() {
+    this.items = this.af.database.list('items');
+  }
+
+  login() {
+    console.log('login');
+    this.af.auth.login(
+      {
+        email: 'user1@b.com',
+        password: 'password'
+      }
+      // ,
+      // {
+      //   provider: AuthProviders.Password,
+      //   method: AuthMethods.Password,
+      // }
+      );
+  }
+
+  logout() {
+    console.log('logout');
+    this.af.auth.logout();
+  }
 }
